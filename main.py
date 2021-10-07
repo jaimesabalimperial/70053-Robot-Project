72edeaf0b9a2dbe68c34e84752488c193bad3e45
Jaime Sabal <js921@ic.ac.uk>
2021-10-07 16:25:31 +0100

    Made program more compact and easy to read

--- main.py
+++ main.py
@@ -1,33 +1,7 @@
 import random as random
-#define size of grid in which robot can move in and direction dictionary
-grid_size = 10
-direction_dict = {"n": "North", "s": "South", "e": "East", "w": "West"}
 
-def intro():
-    """Itroduction to robot program."""
-    name = input("What is the name of the robot? ")
-    identifier = 1000
-
-    message = f"Hello. My name is {name}. My ID is {identifier}."
-    print(message)
-
-def coordinates_func(row, col):
-    """Considers limit cases for when row and column numbers arent within the grid (i.e >grid_size or <0)."""
-    #consider limit cases
-    if row < 0: 
-        row = 0
-    elif row >= grid_size: 
-        row = grid_size - 1
-
-    if col < 0: 
-        col = 0
-    elif col >= grid_size: 
-        col = grid_size - 1
-
-    return (row,col)
-
-def quadrant(coordinates, grid_len=grid_size):
-    """Returns a string with the quadrant the robot is in depending on its coordinates on the grid."""
+def compute_quadrant(coordinates, grid_size=10):
+    #Returns a string with the quadrant the robot is in depending on its coordinates on the grid.
     quadrant_str = ""
     if coordinates[0] < grid_size/2 and coordinates[1] >= grid_size/2: 
         quadrant_str = "top right"
@@ -42,17 +16,44 @@ def quadrant(coordinates, grid_len=grid_size):
 
     return quadrant_str
 
-def location_message(coordinates, direction):
+
+def location_message(coordinates, direction, direction_dict={"n": "North", "s": "South", "e": "East", "w": "West"}):
     print(f"I am currently at {coordinates}, facing {direction_dict[direction]}")
 
 def wall_message():
     print("I have a wall in front of me!")
     print("Turning 90 degreed clockwise.")
 
-def motion(coordinates, direction):
+
+def read_robot_name():
+    """Itroduction to robot program."""
+    name = input("What is the name of the robot? ")
+    identifier = 1000
+
+    message = f"Hello. My name is {name}. My ID is {identifier}."
+    print(message)
+
+
+def coordinates_(row, col, grid_size = 10):
+    """Considers limit cases for when row and column numbers arent within the grid (i.e >grid_size or <0)."""
+    #consider limit cases
+    if row < 0: 
+        row = 0
+    elif row >= grid_size: 
+        row = grid_size - 1
+
+    if col < 0: 
+        col = 0
+    elif col >= grid_size: 
+        col = grid_size - 1
+
+    return (row,col)
+
+
+def navigate(coordinates, direction, target_location=(9,9)):
     """Moves robot one space in the direction specified (or automatically assigned) and returns the
     new coordinates."""
-    drink_location = (9,9) #location of Ribena drink
+
     new_coordinates = coordinates #set new coordinates equal to initia coordinates
 
     #initialise motion
@@ -60,7 +61,7 @@ def motion(coordinates, direction):
     while motion_in_place:
         location_message(new_coordinates, direction)
         #pause motion if location is the same as drink
-        if new_coordinates == drink_location:
+        if new_coordinates == target_location:
             print("I am drinking Ribena! I am happy!")
             break
 
@@ -104,21 +105,34 @@ def motion(coordinates, direction):
 
             new_coordinates = coordinates_func(new_coordinates[0], new_coordinates[1]-1)
     
-    return new_coordinates
 
-if __name__ == "__main__":
-    intro()
+def run_simulation(grid_size=10, target_location=(9,9), direction_dict={"n": "North", "s": "South", "e": "East", "w": "West"}):
+    """ Start robot navigation simulation.
 
-    #user inputs for initial robot coordinate
+    Args:
+        grid_size (int): The size of the grid. Defaults to 10.
+        target_row (int): The target row coordinate. Defaults to 9.
+        target_col (int): The target column coordinate. Defaults to 9.
+    """
+    read_robot_name() #reas robot name and introduce robot with identifier
+
+    #automated inputs for initial robot coordinate
     row, col = random.randint(0, grid_size), random.randint(0, grid_size)
 
-    #initialise robot
+    #initialise robot coordinates (automated)
     coordinates = coordinates_func(row, col)
 
-    #add direction and motion components
-    possible_directions = ["n", "s", "e", "w"]
+    #add random direction and consequent motion
+    possible_directions = list(direction_dict.keys())
     direction = possible_directions[random.randint(0,3)]
-    new_coordinates = motion(coordinates, direction)
+
+    #navigate robot to drink
+    navigate(coordinates, direction)
+
+
+if __name__ == "__main__":
+    #run simulation
+    run_simulation()
 
 
 
